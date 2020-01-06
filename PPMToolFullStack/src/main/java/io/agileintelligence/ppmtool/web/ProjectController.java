e6d5613cc1cc7c8bdbd60a809c7877d3150af649
mengchaoli <lmcwesdxc@163.com>
2020-01-05 22:11:49 -0800

    Add restrictions to project's attribute

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -0,0 +1,25 @@
+package io.agileintelligence.ppmtool.web;
+
+//this is api layer
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
