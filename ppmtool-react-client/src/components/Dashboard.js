340ad74ebc2ac58d59d208cf433bee261e8ea662
mengchaoli <lmcwesdxc@163.com>
2020-01-07 21:05:28 -0800

    Finish create project form

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Projectitem from "./Project/Projectitem";
+import CreateProjectButton from "./Project/CreateProjectButton";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              <Projectitem />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
