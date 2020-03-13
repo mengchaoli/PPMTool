c4cdac1dc7783177e40c4ec80a8ab44c19ced926
mengchaoli <lmcwesdxc@163.com>
2020-03-13 15:48:00 -0700

    Finish project task service and backlog controller

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,7 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -19,6 +21,10 @@ public class ProjectTask {
     private Integer priority;
     private Date dueDate;
     //ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
 
     @Column(updatable = false)
     private  String projectIdentifer;
@@ -117,6 +123,14 @@ public class ProjectTask {
     @PreUpdate
     protected void onUpdate() {
         this.update_At = new Date();
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @Override
---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,48 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Backlog;
+import io.agileintelligence.ppmtool.domain.ProjectTask;
+import io.agileintelligence.ppmtool.repositories.BacklogRepository;
+import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        //PTs to be added to a specific project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //set the backlog to the pt
+        projectTask.setBacklog(backlog);
+        //we want our project sequence to be like this: IDPRO-1, IDPRO-2
+        Integer BacklogSequence = backlog.getPTSquence();
+        //Update the backlog sequence
+        BacklogSequence++;
+
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifer(projectIdentifier);
+
+        //Initial priority when priority is null
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//            projectTask.setPriority(3);
+//        }
+        //Initial status when status is null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+
+    }
+}
